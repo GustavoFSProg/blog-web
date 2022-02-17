1bb960c7ba160a3b4c074c27e6b0422c42138749
Gustavo <gustavo.prog40@gmail.com>
2022-02-17 15:08:08 -0300

    asfasasdf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Container, Button, UlLista } from './styled-app'
 import api from './services/api'
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Header from './Header'
 
 function App() {
